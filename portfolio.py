f059b59648167e469b016167a52bc753e5e6c267
stark10war <shashank.tanwar32@gmail.com>
2020-10-04 16:11:22 +0530

    Start using gitignore

--- portfolio.py
+++ portfolio.py
@@ -1,8 +1,6 @@
 import os
-os.chdir(r"C:\Users\shash\OneDrive\Documents\Shashank\Portfolio_flask")
 from flask import Flask, flash, request, redirect, url_for, session,jsonify, render_template
-from werkzeug.utils import secure_filename
-from waitress import serve
+
 app = Flask(__name__)
 #CORS(app)
 @app.route('/', methods=['GET'])
